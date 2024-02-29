a5c82cd34a90b641e0c49e0f74608128c790888d
Alexandr <sasa20sasha19910@yandex.ru>
2024-02-29 16:31:37 +0300

    добавил логику модели бд в app/users/database

--- app/users/database/settings.py
+++ app/users/database/settings.py
@@ -1,1 +1,20 @@
+from pydantic import AnyUrl
+
+from app.common.database.settings import BaseDatabaseSettings
+
+
+class Settings(BaseDatabaseSettings):
+    """Settings class for the database.
+
+    This class is used to represent the settings required by the database, including the settings inherited from the
+    `BaseDatabaseSettings` class.
+
+    Attributes:
+        dsn (AnyUrl): The data source name for the database. This should be a URL-like string that specifies the type
+                      of database, the host, the port, and the name of the database. The default value is
+                      "sqlite+aiosqlite:///users.sqlite3", which specifies an SQLite database stored in the file
+                      "users.sqlite3".
+    """
+    dsn: AnyUrl = AnyUrl("sqlite+aiosqlite:///users.sqlite3")
+
 
