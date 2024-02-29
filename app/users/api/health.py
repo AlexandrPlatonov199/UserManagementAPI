220fca6f604f624fdeb066f41ba8e4c90c15470b
Alexandr <sasa20sasha19910@yandex.ru>
2024-02-29 16:40:23 +0300

    добавил проверку работоспособности сервиса

--- app/users/api/health.py
+++ app/users/api/health.py
@@ -1,1 +1,17 @@
+from app.common.api.schemas.health import HealthResponse
+from app.common.utils.package import get_version
+
+
+def health() -> HealthResponse:
+    """Check the health of the application.
+
+    This function returns a `HealthResponse` object that indicates the version and name of the application.
+
+    Returns:
+        A `HealthResponse` object with the version and name of the application.
+    """
+    return HealthResponse(
+        version=get_version() or "0.0.0",
+        name="Users",
+    )
 
