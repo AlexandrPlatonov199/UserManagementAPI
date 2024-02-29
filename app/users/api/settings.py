055a4d4f0e3c5eb12cb106807b15df3ec7f14455
Alexandr <sasa20sasha19910@yandex.ru>
2024-02-29 16:17:28 +0300

    добавил settings в app/users/api

--- app/users/api/settings.py
+++ app/users/api/settings.py
@@ -1,1 +1,9 @@
+from app.common.api.settings import BaseAPISettings
 
+
+class Settings(BaseAPISettings):
+    """Settings class for the API.
+
+    This class is used to represent the settings required by the API, including the settings inherited from the
+    `BaseAPISettings` class.
+    """
