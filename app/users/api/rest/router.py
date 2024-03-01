9d6a4151512d30166ba9e0704800e3e656555399
Alexandr <sasa20sasha19910@yandex.ru>
2024-03-01 16:32:20 +0300

    добавил добавил логику router в app/users/api/rest

--- app/users/api/rest/router.py
+++ app/users/api/rest/router.py
@@ -1,1 +1,15 @@
+import fastapi
+
+from . import users
+
+router = fastapi.APIRouter()
+
+"""Router for the API.
+
+This router includes the `users` router, which defines the API endpoints for managing users. The `users` router is
+included with the prefix `/users`, so all of its endpoints will be available at `/users/*`. The `tags` parameter is
+used to specify the tags for the included router, which can be used to group related endpoints together.
+"""
+
+router.include_router(users.router, prefix="/users", tags=["Users"])
 
