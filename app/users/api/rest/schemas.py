cec943129d10c99bdaad77e25664d6c727312ebd
Alexandr <sasa20sasha19910@yandex.ru>
2024-03-01 16:30:53 +0300

    добавил модель ответа pydantic

---
+++ app/users/api/rest/schemas.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, EmailStr
+from app.users.database.models import User
+
+
+class UserResponse(BaseModel):
+    """A response object representing a user.
+
+    Attributes:
+        id (int): The ID of the user.
+        username (str): The username of the user.
+        email (EmailStr): The email address of the user.
+        updated_registration_date (datetime): The date and time the user's registration was last updated.
+        registration_date (datetime): The date and time the user registered.
+    """
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    email: EmailStr
+    updated_registration_date: datetime
+    registration_date: datetime
+
+    @classmethod
+    def from_db_model(cls, user: User) -> "UserResponse":
+        """Create a UserResponse object from a User database model.
+
+        Args:
+            user (User): The User database model.
+
+        Returns:
+            A UserResponse object.
+        """
+        return cls(
+            id=user.id,
+            username=user.username,
+            email=user.email,
+            registration_date=user.registration_date,
+            updated_registration_date=user.updated_registration_date,
+        )
